8322da7597c947a54338c10a0ea8eb5859b26824
Zamir Shaykhislamov <zamir.shaykhislamov@gmail.com>
2022-01-02 23:44:15 +0500

    Add flash alert

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,21 +1,22 @@
 class RegistrationsController < ApplicationController
-    def new
-        @user = User.new
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Successfully created account"
+    else
+      flash[:alert] = "Something was wrong"
+      render :new
     end
+  end
 
-    def create
-        @user = User.new(user_params)
-        if @user.save
-          session[:user_id] = @user.id
-          redirect_to root_path, notice: "Successfully created account"
-        else
-          render :new
-        end
-      end
+  private
 
-    private
-
-    def user_params
-        params.require(:user).permit(:email, :password, :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
