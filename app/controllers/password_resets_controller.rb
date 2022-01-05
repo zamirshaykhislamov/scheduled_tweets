87f10fe999b320e654d387ace013b642e0b5082a
Zamir Shaykhislamov <zamir.shaykhislamov@gmail.com>
2022-01-05 19:31:08 +0500

    add password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -13,4 +13,25 @@ class PasswordResetsController < ApplicationController
     redirect_to root_path, notice: "If an account with that email was found, we have sent a link to reset your password."  
   end
 
+  def edit
+    @user = User.find_signed(params[:token], purpose: "[password_reset")
+  rescue  ActiveSupport::MessageVeryfier::InvalidSignature
+    redirect_to sign_in_path, alert: "Your token has expired. Please try again."
+  end
+
+  def update
+    @user = find_signed!(params[:token], purpose: "password_reset")
+    if @user.update(password_params)
+      redirect_to sign_in_path, notice: "Your password was reset successfully. Please sign in."
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
 end
